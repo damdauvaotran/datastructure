fc870c5e0b061ad597e9f52487920d9d6393c7eb
damdauvaotran <ngokhoaisan1999@gmail.com>
2018-09-19 09:26:18 +0700

    lab 2

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,46 +1,21 @@
 #include <iostream>
-#include <fstream>
-
 using namespace std;
-void insertionSort (int[], int );
+void makeLine(int , int , int , int , int  []);
+
 int main() {
-    ifstream input;
-    input.open("in.txt");
-    int n = 0;
-    int a[1000];
-    if (input) {
-        while (!input.eof()) {
-            input>>a[n];
-            n++;
-        }
+    int x1, y1, x2, y2, x3, y3;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3;
+    int line1[3] = {0};
+    int line2[3] = {0};
+    makeLine(x1,y1,x2,y2,line1);
+    for (int i=0; i< 3; i++) {
+        cout<<line1[i]<< " ";
     }
-    input.close();
-    insertionSort(a,n );
-    for (int i =0; i< n; i++) {
-        cout<<a[i]<<" ";
-    }
-
-
-    ofstream output;
-    output.open("out.txt");
-    for (int i =0; i< n; i++) {
-        output <<a[i]<<" ";
-    }
-    output.close();
-
     return 0;
 }
 
-
-void insertionSort (int a[], int n) {
-    for (int i =1; i<n; i++) {
-        int temp = a[i];
-        int j = i-1;
-        while (temp< a[j]&& j>=0) {
-            a[j+1] = a[j];
-            j--;
-        }
-        a[j+1]= temp;
-
-    }
+void makeLine(int x1, int y1, int x2, int y2, int line[]) {
+    line[0] = y1-y2;
+    line[1] = x2-x1;
+    line [2] = line[0]* (-x1)+ line[1]*(-y1);
 }
